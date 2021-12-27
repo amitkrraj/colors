f9c1bee03a8303f74a2a7a1fc943ca764fd141da
amitkrraj <amitkrraj.cse@gmail.com>
2021-12-27 07:56:57 +0530

    add dark mode feature

--- js/derk.js
+++ js/derk.js
@@ -1,8 +1,8 @@
 const options = {
-    bottom: "16px", // default: '32px'
-    right: "19px", // default: '32px'
+    bottom: "3px", // default: '32px'
+    right: "17px", // default: '32px'
     left: "unset", // default: 'unset'
-    time: "0.5s", // default: '0.3s'
+    time: "0.9s", // default: '0.3s'
     mixColor: "#fff", // default: '#fff'
     backgroundColor: "#fff", // default: '#fff'
     buttonColorDark: "#100f2c", // default: '#100f2c'
